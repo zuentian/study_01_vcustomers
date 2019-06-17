4d96ab420f2f7ef2806494d269ae56613e75827b
zuer <1974900537@qq.com>
2019-06-17 22:01:11 +0800

    修复svg图片不能显示的

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -42,14 +42,6 @@ module.exports = {
         include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
       },
       {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
-        }
-      },
-      {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
         loader: 'url-loader',
         options: {
